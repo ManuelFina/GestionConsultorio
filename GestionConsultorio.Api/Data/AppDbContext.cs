228f9b9eb4b53f2c8395d1bdc0660d25e561ef80
Manuel Fina <finamanuelmateo@gmail.com>
2026-06-23 19:12:37 -0300

    feat(api): configurar appdbcontext y conexion sql server

---
+++ GestionConsultorio.Api/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using GestionConsultorio.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestionConsultorio.Api.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Paciente> Pacientes { get; set; }
+    public DbSet<Medico> Medicos { get; set; }
+    public DbSet<Especialidad> Especialidades { get; set; }
+    public DbSet<Consultorio> Consultorios { get; set; }
+    public DbSet<Turno> Turnos { get; set; }
+    public DbSet<HistorialClinico> HistorialesClinicos { get; set; }
+    public DbSet<Usuario> Usuarios { get; set; }
+}
--- GestionConsultorio.Api/Program.cs
+++ GestionConsultorio.Api/Program.cs
@@ -1,3 +1,6 @@
+using GestionConsultorio.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
- //dbcon
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -14,28 +20,4 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
