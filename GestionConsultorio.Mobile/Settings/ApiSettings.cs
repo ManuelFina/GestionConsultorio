c93e6767652596a11ec0dc3a4ad8a93a1aa23d67
Manuel Fina <finamanuelmateo@gmail.com>
2026-07-03 11:34:16 -0300

    chore(release): preparar app para pruebas en dispositivo físico

--- GestionConsultorio.Mobile/Settings/ApiSettings.cs
+++ GestionConsultorio.Mobile/Settings/ApiSettings.cs
@@ -2,10 +2,9 @@
 
 public static class ApiSettings
 {
-    //el front y la API en la misma máquina
-    public const string BaseUrl = "http://localhost:5186/";
+    /*el front y la API en la misma máquina
+    public const string BaseUrl = "http://localhost:5186/";*/
 
-    /* Emulador 
-    public const string BaseUrl = "http://10.0.2.2:5186/";
-    */
+    public const string BaseUrl = "http://192.168.0.2:5186/";
+    
 }
