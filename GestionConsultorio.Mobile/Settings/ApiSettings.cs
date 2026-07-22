29c80dc812f5c47f3e64a3e54f1b5eaa85facd9e
Manuel Fina <finamanuelmateo@gmail.com>
2026-07-22 19:03:35 -0300

    chore: finalizar ajustes de despliegue

--- GestionConsultorio.Mobile/Settings/ApiSettings.cs
+++ GestionConsultorio.Mobile/Settings/ApiSettings.cs
@@ -2,9 +2,5 @@
 
 public static class ApiSettings
 {
-    /*el front y la API en la misma máquina
-    public const string BaseUrl = "http://localhost:5186/";*/
-
-    public const string BaseUrl = "http://192.168.0.2:5186/";
-    
+    public const string BaseUrl = "http://www.GestionConsultorio.somee.com/";
 }
